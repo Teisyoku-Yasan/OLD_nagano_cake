71c719799b1cf7d2a7bcc156e53ba55e38c84ea8
vagrant <vagrant@localhost.localdomain>
2020-08-13 05:38:39 +0000

    first-commit

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Devise 管理者用ルーティング
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  # Devise ユーザ用ルーティング
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  # メインページルーティング
+  root 'home#index'
+
+
+
+
+
+
+end
